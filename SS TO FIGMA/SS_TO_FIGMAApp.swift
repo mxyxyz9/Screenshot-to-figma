e1c2e21db6f10faa83c4cca5dfc3d27b42b2a68f
Rushil <121330302+mxyxyz9@users.noreply.github.com>
2025-07-02 20:58:11 +0530

    Initial Commit

---
+++ SS TO FIGMA/SS_TO_FIGMAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SS_TO_FIGMAApp.swift
+//  SS TO FIGMA
+//
+//  Created by Pala Rushil on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SS_TO_FIGMAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
